dcea2c8f432716ec54efe8ade2f9f3e3276ec84d
pachiclana <javiermg1986@gmail.com>
2024-04-01 19:19:38 +0200

    added timestamp to python script

--- multi-platform-tests/src/main.py
+++ multi-platform-tests/src/main.py
@@ -1,7 +1,10 @@
 import time
 import sys
+import datetime 
+now = datetime.datetime.now()
 
 print(sys.version)
+print(now.strftime('%Y-%m-%dT%H:%M:%S') + ('-%02d' % (now.microsecond / 10000)))
 
 while True:
     print("Docker is working!")
